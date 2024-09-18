6bc4ef3613625daf6612121085106af3efb7f2c8
OlenaBoichenko <ol.boichenko.dev@gmail.com>
2024-09-17 23:11:50 -0400

    add localstorage and styles

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,14 +1,46 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(""); // user enters a movie title
+  const [movies, setMovies] = useState([]); // list of movies
+  const [query, setQuery] = useState(""); // current request
 
-  const handleSearch = async () => {
-    const response = await axios.get(`http://localhost:5000/api/movies?query=${searchTerm}`);
-    setMovies(response.data.Search || []);
+  // Function to perform a search
+  const handleSearch = useCallback(async () => {
+    if (query.trim() !== "") {
+      // only if the request is non-empty
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/api/movies?query=${query}`
+        );
+        const fetchedMovies = response.data.Search || [];
+        setMovies(fetchedMovies);
+
+        // save data in localStorage
+        localStorage.setItem("movies", JSON.stringify(fetchedMovies));
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    }
+  }, [query]);
+
+  useEffect(() => {
+    handleSearch();
+  }, [handleSearch]);
+
+  // loading saved movies from localStorage
+  useEffect(() => {
+    const savedMovies = localStorage.getItem("movies");
+    if (savedMovies) {
+      const parsedMovies = JSON.parse(savedMovies);
+      setMovies(parsedMovies); // Устанавливаем фильмы из localStorage, если они там есть
+    }
+  }, []);
+
+  const handleButtonClick = () => {
+    setQuery(searchTerm);
   };
 
   return (
@@ -20,10 +52,10 @@ const HomePage = () => {
         onChange={(e) => setSearchTerm(e.target.value)}
         placeholder="Search for a movie..."
       />
-      <button onClick={handleSearch}>Search</button>
-      <div className='movie-list'>
-        {movies.map(movie => (
-          <div key={movie.imdbID} className='movie-item'>
+      <button onClick={handleButtonClick}>Search</button>
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <div key={movie.imdbID} className="movie-item">
             <h3>{movie.Title}</h3>
             <img src={movie.Poster} alt="poster" />
             <Link to={`/movie/${movie.imdbID}`}>More Details</Link>
